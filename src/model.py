32955f34e9000b35917d2acf48bf3f4ff6c09544
Egor Andreasyan <124351915+EgorAndrik@users.noreply.github.com>
2024-07-07 12:42:46 +0700

    Update model.py
    add coments

--- src/model.py
+++ src/model.py
@@ -12,7 +12,17 @@ import os, cv2
 import matplotlib.pyplot as plt
 
 
-def transform(image, mas_points):
+def transform(image: np.ndarray, mas_points: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    Применяет перспективное преобразование к изображению на основе заданных точек.
+
+    Args:
+        image (np.ndarray): Входное изображение.
+        mas_points (np.ndarray): Массив точек для перспективного преобразования.
+
+    Returns:
+        Tuple[np.ndarray, np.ndarray]: Преобразованное изображение и измененное исходное изображение.
+    """
     width, height = 512, 112
     image = cv2.resize(image, (192, 64), interpolation=cv2.INTER_LINEAR)
     pts2 = np.float32([[0, 0], [width, 0], [width, height], [0, height]])
@@ -26,7 +36,16 @@ def transform(image, mas_points):
     return result, image
 
 
-def order_points(pts):
+def order_points(pts: np.ndarray) -> np.ndarray:
+    """
+    Упорядочивает точки в последовательности: верхний левый, верхний правый, нижний правый, нижний левый.
+
+    Args:
+        pts (np.ndarray): Массив точек.
+
+    Returns:
+        np.ndarray: Упорядоченные точки.
+    """
     rect = np.zeros((4, 2), dtype="float32")
     s = pts.sum(axis=1)
     diff = np.diff(pts, axis=1)
@@ -37,35 +56,15 @@ def order_points(pts):
     return rect
 
 
-class SegmentationDataset(Dataset):
-    def __init__(self, dataframe: pd.DataFrame, transform=None, mask_transform=None, train=False) -> None:
-        self.dataframe = dataframe
-        self.transform = transform
-        self.mask_transform = mask_transform
-        self.train = train
-
-    def __len__(self):
-        return len(self.dataframe)
-
-    def __getitem__(self, idx):
-        img_path = self.dataframe.iloc[idx, 0]
-        if not os.path.exists(img_path):
-            raise FileNotFoundError(f"Image file {img_path} not found.")
-        image = Image.open(img_path).convert('RGB')
-        if self.transform:
-            image = self.transform(image)
-        if self.train:
-            mask_rle = self.dataframe.iloc[idx, 1]
-            mask = rle_decode(mask_rle, (image.height, image.width))
-            if self.mask_transform:
-                mask = self.mask_transform(mask)
-            else:
-                mask = torch.tensor(mask, dtype=torch.float32).unsqueeze(0)
-        return image, mask if self.train else image
-
-
 class SegmentCarNumber:
     def __init__(self, unet_path: str, encoder_path: str) -> None:
+        """
+        Инициализирует модель SegmentCarNumber с заданными путями к моделям UNet и энкодеру.
+
+        Args:
+            unet_path (str): Путь к контрольной точке модели UNet.
+            encoder_path (str): Путь к контрольной точке энкодера.
+        """
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
         self.model = smp.Unet(
@@ -89,7 +88,14 @@ class SegmentCarNumber:
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ])
 
-    def predict_dir(self, dir_imgs: str, output_path: str):
+    def predict_dir(self, dir_imgs: str, output_path: str) -> None:
+        """
+        Предсказывает и выравнивает изображения из заданного каталога, сохраняет результаты в выходной каталог.
+
+        Args:
+            dir_imgs (str): Каталог с входными изображениями.
+            output_path (str): Каталог для сохранения выходных изображений.
+        """
         self.model.eval()
         path_list = [os.path.join(dir_imgs, f) for f in os.listdir(dir_imgs)]
         ind = 0
@@ -106,6 +112,15 @@ class SegmentCarNumber:
             ind += 1
 
     def predict(self, image: str | Image.Image) -> np.ndarray:
+        """
+        Предсказывает и выравнивает одно изображение.
+
+        Args:
+            image (Union[str, Image.Image]): Путь к изображению или PIL Image.
+
+        Returns:
+            np.ndarray: Предсказанная маска для изображения.
+        """
         self.model.eval()
         if isinstance(image, str):
             image = Image.open(image).convert('RGB')
@@ -120,6 +135,16 @@ class SegmentCarNumber:
     
     @staticmethod
     def perspective_transform_rgb(image_rgb: np.ndarray, mask_img: np.ndarray) -> np.ndarray:
+        """
+        Применяет перспективное преобразование к данному RGB изображению на основе маски изображения.
+
+        Args:
+            image_rgb (np.ndarray): Входное RGB изображение.
+            mask_img (np.ndarray): Маска изображения для преобразования.
+
+        Returns:
+            np.ndarray: Преобразованное RGB изображение.
+        """
         image_rgb = cv2.resize(image_rgb, (192, 64), interpolation=cv2.INTER_LINEAR)
         _, binary_mask = cv2.threshold(mask_img, 200, 255, cv2.THRESH_BINARY)
         binary_mask = cv2.convertScaleAbs(binary_mask)
@@ -168,6 +193,16 @@ class SegmentCarNumber:
         return [transformed_image_resized]
     
     def binary_model(self, image, your_image):
+        """
+        Применяет бинарное пороговое значение и находит контуры для выполнения перспективного преобразования.
+
+        Args:
+            image (np.ndarray): Предсказанная бинарная маска.
+            your_image (np.ndarray): Исходное изображение.
+
+        Returns:
+            List[np.ndarray]: Список, содержащий преобразованное изображение.
+        """
         plug_image = image
         image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -178,14 +213,13 @@ class SegmentCarNumber:
         for contour in contours:
             epsilon = 0.02 * cv2.arcLength(contour, True)
             approx = cv2.approxPolyDP(contour, epsilon, True)
-            if len(approx) == 4:  # Проверяем, что аппроксимированный контур имеет 4 вершины
+            if len(approx) == 4:
                 cv2.drawContours(output_image, [approx], -1, (0, 255, 0), 3)
                 for point in approx:
                     mas_points.append(point[0])
         if len(mas_points) == 4:
             mas_points = np.array(mas_points, dtype=np.float32)
-            mas_points = order_points(mas_points)  # Использование функции order_points для сортировки
-            # print(mas_points)
+            mas_points = order_points(mas_points)
             return transform(your_image, mas_points)
         else:
             return self.perspective_transform_rgb(your_image, plug_image)
